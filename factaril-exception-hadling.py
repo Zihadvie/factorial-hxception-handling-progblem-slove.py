548fb413e9b20ae72f1cf99a09ca45e74d94cb14
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2026-05-13 18:18:23 +0600

    factorial-exception-handling
    #critical progblem slove use python

--- factaril-exception-hadling.py
+++ factaril-exception-hadling.py
@@ -1,1 +1,47 @@
+# Question 1 — Factorial + Exception Handling
+#
+# Take a number as input from the user.
+#
+# Rules:
+#
+# If the user enters a string → Invalid Input
+#
+# If the user enters a negative number → Factorial possible na
+#
+# Otherwise, calculate the factorial using a function.
+#
+# Display the final result in 2 lines:
+#
+# The factorial value.
+#
+# Whether the number is even or odd.
+try :
+
+        n = int(input())
+        if (n > 0):
+            for a in range(1, n + 1):
+
+             r = 1
+            for b in range(1, a + 1):
+                r = r * b
+            print("Factorial value : ", r)
+
+
+
+            if r % 2 == 0:
+
+                print("Number even")
+            else:
+                print("Number odd")
+        else:
+            print("Factorial possible na")
+except Exception as e:
+               print(e)
+
+finally:
+    print("wow")
+
+
+
+
 
